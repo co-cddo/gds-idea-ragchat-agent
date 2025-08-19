beaac608a152eae15325304b4cc17365002f6d7c
David Gillespie <david.gillespie@digital.cabinet-office.gov.uk>
2025-08-19 09:45:05 +0100

    add ability to pass in model kwargs

--- src/rag_chat_agent/aws/bedrock.py
+++ src/rag_chat_agent/aws/bedrock.py
@@ -6,17 +6,28 @@ logger = logging.getLogger(__name__)
 
 
 class BedrockHandler:
-    def __init__(self, config):
+    def __init__(self, config, model_kwargs: None | dict):
         logger.info("Initializing BedrockHandler")
         self.region = config.LLM_REGION
         self.model_id = config.LLM_MODEL
+
+        if model_kwargs and not isinstance(model_kwargs, dict):
+            logger.error("model_kwargs should be a dict[str]")
+            raise ValueError("model_kwargs should be a dict[str]")
+
+        self.model_kwargs = model_kwargs
+
         logger.debug(f"LLM Region: {self.region}")
         logger.debug(f"LLM Model ID: {self.model_id}")
 
     def get_llm(self):
         logger.info("Creating ChatBedrock LLM instance")
         try:
-            llm = ChatBedrock(model_id=self.model_id, region_name=self.region)
+            llm = ChatBedrock(
+                model_id=self.model_id,
+                region_name=self.region,
+                model_kwargs=self.model_kwargs,
+            )
             logger.info("Successfully created ChatBedrock LLM instance")
             return llm
         except Exception as e:
